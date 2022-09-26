48109a1cd0bd331a74a6d8e0234461bf8d956890
IagoMuhl <iagomuhl17@gmail.com>
2022-09-26 17:23:58 -0300

    tentativa de base

---
+++ normal.f90
@@ -0,0 +1,67 @@
+program normal
+    implicit none
+    integer, parameter:: minConfig=2
+    integer, parameter:: maxConfig= minConfig**4
+    integer:: k
+    integer:: s1,s2,s3,s4,m1,m2,m3,m4
+    real*8:: s(4,64),H,J1,J2,J3
+
+    J3=0.05d0
+    J2=0.3d0
+    J1=1.d0
+    k=1
+    !!!!!!!!!!    CONTRUÇÃO DA BASE     !!!!!!!!!!!!
+    do s1 =-1,1,2
+    do s2 =-1,1,2
+    do s3 =-1,1,2
+    do s4 =-1,1,2
+                    
+        s(1,k)=s1
+        s(2,k)=s2
+        s(3,k)=s3
+        s(4,k)=s4
+
+        m1=s1 
+        m2=s2
+        m3=s3
+        m4=s4
+
+        print*, s1,s2,s3,s4    
+
+            !!!!!!!!!!!!!!!!! HAMILTONIANO !!!!!!!!!!!!!!
+
+            H = -J1*(((s1*m2)-(m1*m2)/2.d0) &
+                    &+(s1*m3)-(m1*m3)/2.d0) & 
+            &-(3*J2)*((s1*m4)-(m1*m4)/2.d0)&
+            &-(4*J3)*((s1*m1)-((m1)**2.d0)/2.d0)
+
+
+            H = H +J1*(((s2*m1)-(m2*m1)/2.d0)&
+            &+(s2*m4)-(m2*m4)/2.d0)&
+            &+(3*J2)*((s2*m3)-(m2*m3/2.d0))&
+            &+(4*J3)*((s2*m2)-((m2)**2.d0)/2.d0)
+
+
+            H = H -J1*(((s3*m1)-(m3*m1)/2.d0) &
+                    &+(s3*m4)-(m3*m4)/2.d0) & 
+            &-(3*J2)*((s3*m2)-(m3*m2)/2.d0)&
+            &-(4*J3)*((s3*m3)-((m3)**2.d0)/2.d0)
+
+
+            H = H +J1*(((s4*m2)-(m4*m2)/2.d0)&
+            &+(s4*m3)-(m4*m3)/2.d0)&
+            &+(3*J2)*((s4*m1)-(m4*m1/2.d0))&
+            &+(4*J3)*((s4*m4)-((m4)**2.d0)/2.d0)
+
+        !print*, H
+
+        k=k+1
+
+    end do
+    end do
+    end do
+    end do
+
+
+
+end program normal
