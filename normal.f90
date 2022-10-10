012d2e33fa769b24653310b23edbff324e39b575
IagoMuhl <iagomuhl17@gmail.com>
2022-10-10 19:12:11 -0300

    hamilton ok

--- normal.f90
+++ normal.f90
@@ -5,33 +5,24 @@ program normal
    integer, parameter:: num_sites= 4
    integer, parameter:: maxConfig= minConfig**num_sites
    integer, dimension(maxConfig,num_sites) :: s
-   integer :: i
-   real, parameter:: J1=1.,J2=0.5,J3=0
-   real, dimension(maxConfig):: H_intra
-   real, dimension(maxConfig):: H_inter
+   real, parameter:: J1=1.,J2=0.,J3=0
+   real, dimension(maxConfig):: H_intra, H_inter, H
+   real, dimension(num_sites):: m_guess
+
    call base(s)
 
-   !---------------------HAMILTONIANO INTRA-----------------------------
-   do i = 1, maxConfig
-      H_intra(i) = J1*(s(i,1)*s(i,2) + s(i,1)*s(i,3) + s(i,4)*s(i,3) + s(i,2)*s(i,4)) &
-      & + J2*(s(i,1)*s(i,4) + s(i,3)*s(i,2))
+   call HAM_INTRA(J2,s,H_intra)
 
-   end do
-   !---------------------HAMILTONIANO INTRA-----------------------------
+      m_guess = [1.,-1.,-1.,1.]
 
-   do i = 1, maxConfig
-      H_inter(i) = J1*(((s(i,1)*s(i,2))-(s(i,1)*s(i,2))/2.) &
-                   & + ((s(i,1)*s(i,3))-(s(i,1)*s(i,3))/2.)) &
-      & + (3*J2)*((s(i,1)*s(i,4))-(s(i,1)*s(i,4))/2.) &
-      & + (4*J3)*((s(i,1)**2)-((s(i,1))**2.)/2.)
-   end do
+   call HAM_INTER(J2,J3,s,m_guess,H_inter)
+
+   H = H_intra + H_inter
 
 
+   !call print_matrix(maxConfig,num_sites,s)
 
-
-   call print_matrix(maxConfig,num_sites,s)
-
-   call print_matrixH(maxConfig,1,H_intra)
+   call print_matrixH(maxConfig,1,H)
 
 end program normal
 
@@ -70,6 +61,70 @@ subroutine base(s)
       end do
    end do
 
+
+end subroutine
+
+subroutine HAM_INTRA(J2,s,H_intra)
+   integer, parameter:: minConfig= 2
+   integer, parameter:: num_sites= 4
+   integer, parameter:: maxConfig= minConfig**num_sites
+   integer, dimension(maxConfig,num_sites), intent(in):: s
+   integer :: i
+   real, intent(in):: J2
+   real, parameter:: J1=1.
+   real, dimension(maxConfig), intent(out):: H_intra
+
+   !---------------------HAMILTONIANO INTRA-----------------------------
+   do i = 1, maxConfig
+      H_intra(i) = J1*(s(i,1)*s(i,2) + s(i,1)*s(i,3) + s(i,4)*s(i,3) + s(i,2)*s(i,4)) &
+      & + J2*(s(i,1)*s(i,4) + s(i,3)*s(i,2))
+
+   end do
+   !---------------------HAMILTONIANO INTRA-----------------------------
+
+   ! do i = 1, maxConfig
+   !  H_inter(i) = J1*(((s(i,1)*s(i,2))-(s(i,1)*s(i,2))/2.) &
+   !  & + ((s(i,1)*s(i,3))-(s(i,1)*s(i,3))/2.)) &
+   !  & + (3*J2)*((s(i,1)*s(i,4))-(s(i,1)*s(i,4))/2.) &
+   !   & + (4*J3)*((s(i,1)**2)-((s(i,1))**2.)/2.)
+   ! end do
+
+end subroutine
+
+subroutine HAM_INTER(J2,J3,s,mag,H_inter)
+   integer, parameter:: minConfig= 2
+   integer, parameter:: num_sites= 4
+   integer, parameter:: maxConfig= minConfig**num_sites
+   integer, dimension(maxConfig,num_sites), intent(in):: s
+   integer :: i
+   real, intent(in):: J2,J3
+   real, parameter:: J1=1.
+   real, dimension(num_sites), intent(in):: mag
+   real, dimension(maxConfig), intent(out):: H_inter
+ 
+
+    do i = 1, maxConfig
+      H_inter(i) = J1*(((s(i,1)*mag(2))-(mag(1)*mag(2))/2.) &
+    & +                ((s(i,1)*mag(3))-(mag(1)*mag(3))/2.)) &
+    & +         (3*J2)*((s(i,1)*mag(4))-(mag(1)*mag(4))/2.) &
+      & +       (4*J3)*((s(i,1)*mag(1))-((mag(1))**2)/2.) +&
+
+      &            J1*(((s(i,2)*mag(4))-(mag(2)*mag(4))/2.) &
+    & +                ((s(i,2)*mag(1))-(mag(2)*mag(1))/2.)) &
+    & +         (3*J2)*((s(i,2)*mag(3))-(mag(2)*mag(3))/2.) &
+      & +       (4*J3)*((s(i,2)*mag(2))-((mag(2))**2)/2.) +&
+
+      &            J1*(((s(i,3)*mag(1))-(mag(3)*mag(1))/2.) &
+    & +                ((s(i,3)*mag(4))-(mag(3)*mag(4))/2.)) &
+    & +         (3*J2)*((s(i,3)*mag(2))-(mag(3)*mag(2))/2.) &
+      & +       (4*J3)*((s(i,3)*mag(3))-((mag(3))**2)/2.) +&
+
+      &            J1*(((s(i,4)*mag(2))-(mag(4)*mag(2))/2.) &
+    & +                ((s(i,4)*mag(3))-(mag(4)*mag(3))/2.)) &
+    & +         (3*J2)*((s(i,4)*mag(1))-(mag(4)*mag(1))/2.) &
+      & +       (4*J3)*((s(i,4)*mag(4))-((mag(4))**2)/2.)
+
+    end do
 
 end subroutine
 
