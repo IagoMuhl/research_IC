9bf526fdd67880cbff5762bc8dfecb664e2f7e90
IagoMuhl <iagomuhl17@gmail.com>
2023-04-24 19:03:59 -0300

    magnetização copada

--- tarefas/2-nano_curso.f90
+++ tarefas/2-nano_curso.f90
@@ -2,15 +2,14 @@ program nano2
    use QUANTICO
    implicit none
 
-   real*8, parameter:: J2 = 0.d0
+   real*8, parameter:: J2 = 0.5d0
    real*8, dimension(2,2):: sigma_z, Id
-   real*8, dimension(2*2,2*2):: sig_zz, Id_2, sig_Idz, sig_zId
+   real*8, dimension(2*2,2*2):: sig_zz, Id_2, Id_sig_z, sig_z_Id
    real*8, dimension(2**4,2**4):: F , H_1, H_2, H_intra, Id_4, H_inter, Ham
    real*8, dimension(2**4,2**4):: s1, s2, s3, s4
-   real*8:: Z, T, T_final, step, mag, tol, erro
-   real*8, dimension(4):: m_guess
+   real*8:: Z, T, T_final, step, m, tol, erro, m_guess
    character(len=3):: state
-   integer:: m
+   integer:: dim
 
    ! write(*,*) 'Qual estado desejas calcular?(AF,PM,SAF)'
    ! read(*,*) state
@@ -18,50 +17,55 @@ program nano2
 !---------------------------------------------------------
 !DECLARAÇÃO DE VALORES
 
-   H_1 = 0; H_2 = 0; T = 0.5d0; T_final = 4.d0; 
+   H_1 = 0; H_2 = 0;
+   
+   T = 0.5d0; T_final = 4.d0; 
+
    step = 10.d0**(-3); tol = 10.d0**(-8)
-   mag = 1.d0; m = 2
+
+   m_guess = 1.d0; dim = 2; 
 
 
 
-   sigma_z = reshape([1,0,0,-1],[m,m])
+   sigma_z = reshape([1,0,0,-1],[dim,dim])
 
-   Id = reshape([1,0,0,1],[m,m])
+   Id = reshape([1,0,0,1],[dim,dim])
 
 !---------------------------------------------------------
 ! CALCULO DAS POSSIBILIDADES DE SIGMA-Z E IDENTIDADE
 
- call tensorial(sigma_z,sigma_z,m,sig_zz)
- call tensorial(Id,Id,m,Id_2)
- call tensorial(sigma_z,Id,m,sig_zId)
- call tensorial(Id,sigma_z,m,sig_Idz)
+ call tensorial(sigma_z,sigma_z,dim,sig_zz)
+ call tensorial(Id,Id,dim,Id_2)
+ call tensorial(sigma_z,Id,dim,sig_z_Id)
+ call tensorial(Id,sigma_z,dim,Id_sig_z)
 
- call tensorial(sig_zId,Id_2,m*m,s1)
- call tensorial(sig_Idz,Id_2,m*m,s2)
- call tensorial(Id_2,sig_zId,m*m,s3)
- call tensorial(Id_2,sig_Idz,m*m,s4)
+ call tensorial(sig_z_Id,Id_2,dim*dim,s1)
+ call tensorial(Id_sig_z,Id_2,dim*dim,s2)
+ call tensorial(Id_2,sig_z_Id,dim*dim,s3)
+ call tensorial(Id_2,Id_sig_z,dim*dim,s4)
 
- call tensorial(Id_2,Id_2,m*m,Id_4)
+ call tensorial(Id_2,Id_2,dim*dim,Id_4)
 !---------------- HAMILTONIANA J1-------------------------
 
-                   
- call tensorial(sig_zz,Id_2,m*m,F)
+   !S1*S2
+ call tensorial(sig_zz,Id_2,dim*dim,F)
 
    H_1 = H_1 + F
 
 
-
-   call tensorial(sig_zId,sig_zId,m*m,F)
+   !S1*S3
+   call tensorial(sig_z_Id,sig_z_Id,dim*dim,F)
 
    H_1 = H_1 + F
 
  
-
-   call tensorial(sig_Idz,sig_Idz,m*m,F)
+   !S2*S4
+   call tensorial(Id_sig_z,Id_sig_z,dim*dim,F)
 
    H_1 = H_1 + F
 
-   call tensorial(Id_2,sig_zz,m*m,F)
+   !S3*S4
+   call tensorial(Id_2,sig_zz,dim*dim,F)
 
    H_1 = H_1 + F
 
@@ -69,11 +73,11 @@ program nano2
 !---------------- HAMILTONIANA J2-------------------------
    
 
-   call tensorial(sig_zId,sig_Idz,m*m,F)
+   call tensorial(sig_z_Id,Id_sig_z,dim*dim,F)
 
    H_2 = H_2 + F
 
-   call tensorial(sig_Idz,sig_zId,m*m,F)
+   call tensorial(Id_sig_z,sig_z_Id,dim*dim,F)
 
    H_2 = H_2 + F
 
@@ -81,58 +85,50 @@ program nano2
 !---------------------------------------------------------
 
    H_intra = J1*H_1 + J2*H_2
-
    
 
 do while (T<=T_final) !FUNÇÃO DE PARTIÇÃO/ LOOP TEMPERATURA
-   
-   call mag_vetor(state,mag,m_guess)
-   
+
    call Ham_inter_state(state,J2,s1,s2,s3,s4,m_guess,Id_4,H_inter)
 
    Ham = H_intra + H_inter
    
-   call partition(Ham,T,m,Z)
-   
-   call magnetization(Ham,Z,T,m,s1,mag)
-   
-   ERRO = abs(mag - m_guess(1))
+   call partition(Ham,T,dim,Z)
 
-   !print*, T, ERRO, mag, m_guess(1)
-   call print_matrix(s1,m**4)
-   call print_matrix(Ham,m**4)
-   read(*,*)
+   call magnetization(Ham,Z,T,dim,s1,m)
+
+
+   ERRO = abs(m_guess - m)
+
       do while (ERRO >= tol)
+               
+         m_guess = m
 
-         !print*, T, mag, m_guess(1), erro
+         call Ham_inter_state(state,J2,s1,s2,s3,s4,m_guess,Id_4,H_inter)
          
-
-         call mag_vetor(state,mag,m_guess)
-
-         call Ham_inter_state(state,J2, s1,s2,s3,s4,m_guess,Id_4,H_inter)
-
          Ham = H_intra + H_inter
-
-         call partition(Ham,T,m,Z)
-
-
-         call magnetization(H_inter,Z,T,m,s1,mag)
-
-         ERRO = abs(mag - m_guess(1))
-
-         print*, T, mag, m_guess(1), erro
-         read(*,*)
+         
+         call partition(Ham,T,dim,Z)
+        
+         call magnetization(Ham,Z,T,dim,s1,m)
+         !print*, m
+         !read(*,*)
+         ERRO = abs(m_guess - m)
+ 
       end do
 
+      !print*, T,m
+
+      if (m<=10.d0**(-5)) then
+         print*, T, m
+         exit
+      end if
    T = T + step
 
-   !write(10,*) T, mag
-
-   !print *, 'T =',T,'Z =',Z, 'MAG =',abs(m_guess(1))
+   write(10,*) T, m
 
 end do
 
 
-!print *, 'T=',T,'Z=',Z, 'MAG=',mag
-
+!print *, 'T=',T,'Z=',Z, 'm=',m
 end program
--- tarefas/Quant_module.f90
+++ tarefas/Quant_module.f90
@@ -1,131 +1,133 @@
 module QUANTICO
    implicit none
    integer, parameter:: J1=1
-   contains
+contains
 
-   subroutine print_matrix(A,m)
+   subroutine print_matrix(A,dim)
       implicit none
-      integer, intent(in):: m
-      real*8, dimension(m,m), intent(in):: A
+      integer, intent(in):: dim
+      real*8, dimension(dim,dim), intent(in):: A
       integer:: i, j
-      do i=1,m
-         write(*,20)(A(i,j),j=1,m) !para numeros inteiros (4(I3))
-         20 format (16(F3.0))
+      do i=1,dim
+         write(*,20)(A(i,j),j=1,dim) !para numeros inteiros (4(I3))
+20       format (16(F3.0))
       enddo
    end subroutine
-   
-   subroutine tensorial(A,B,m,E)
+
+   subroutine tensorial(A,B,dim,E)
       implicit none
-      integer, intent(in):: m
-      real*8, dimension(m,m), intent(in):: A
-      real*8, dimension(m,m), intent(in):: B
-      real*8, dimension(m**2,m**2), intent(out):: E
+      integer, intent(in):: dim
+      real*8, dimension(dim,dim), intent(in):: A
+      real*8, dimension(dim,dim), intent(in):: B
+      real*8, dimension(dim**2,dim**2), intent(out):: E
       integer:: i, j, k, l, alfa, beta
-   
-      do i = 1,m
-         do j = 1,m
-            do k = 1,m
-               do l = 1,m
-   
-                  alfa = m*(i-1) + k
-                  beta = m*(j-1) + l
-   
+
+      do i = 1,dim
+         do j = 1,dim
+            do k = 1,dim
+               do l = 1,dim
+
+                  alfa = dim*(i-1) + k
+                  beta = dim*(j-1) + l
+
                   E(alfa,beta) = A(i,j) * B(k,l)
-   
+
                end do
             end do
          end do
       end do
    end subroutine
-   
-   subroutine partition(H,T,m,Z)
+
+   subroutine partition(H,T,dim,Z)
+      implicit none
       real*8, intent(out):: Z
-      integer, intent(in):: m
-      real*8, dimension(m**4,m**4), intent(in):: H
+      integer, intent(in):: dim
+      real*8, dimension(dim**4,dim**4), intent(in):: H
       real*8, intent(in):: T
       real*8:: b
       integer :: i
       b = 1.d0/T
       Z = 0.d0
-   
-   
-      do i = 1, m**4
+
+
+      do i = 1, dim**4
          Z = Z + (dexp(-b*(H(i,i))))
       end do
-      
-   end subroutine
-   
-   subroutine magnetization(H,Z,T,m,s1,mag)
-      real*8:: b
-      integer, intent(in):: m
-      real*8, dimension(m**4,m**4), intent(in):: H,s1
-      real*8, intent(out):: mag
-      real*8, intent(in):: T, Z
-      integer :: i
-     
-      
-   
-      b = 1.d0/T
-      mag = 0
 
-      do i = 1, m**4
-         PRINT*, mag
-         mag = mag + s1(i,i)*(dexp(-b*(H(i,i))))
-         !READ(*,*)
-         !print*, 'HA MULQKE',i
-      end do
-   
-      mag = mag/Z
- 
    end subroutine
 
-   subroutine mag_vetor(state,sigma,mag)
+   subroutine magnetization(H,Z,T,dim,s1,m)
       implicit none
-      character(len=*), intent(in):: state
-      real*8, intent(in):: sigma
-      real*8, dimension(4), intent(out):: mag
+      integer, intent(in):: dim
+      real*8, dimension(dim**4,dim**4), intent(in):: H,s1
+      real*8, intent(in):: T, Z
+      real*8, intent(out):: m
+      integer :: i
+      real*8:: b
 
 
-      if ( state=='AF' ) then
-         mag = [sigma, -sigma, -sigma, sigma]
-      else if (state=='SAF') then
-         mag = [sigma, -sigma, sigma, -sigma]
-      else if (state=='PM') then
-         mag = 0.d0
-      else if (state=='SD') then
-         mag = [sigma, sigma, -sigma, -sigma]
-      end if
+
+      b = 1.d0/T
+      m = 0
+
+      do i = 1, dim**4
+         m = m + s1(i,i)*(dexp(-b*(H(i,i))))
+
+         !print*, m, H(i,i), T
+         !read(*,*)
+
+      end do
+      m = m/Z
+      !PRINT*, m, Z
+      ! READ(*,*)
+      ! PRINT*, m
+      ! READ(*,*)
+   end subroutine
+
+    subroutine mag_vetor(state,sigma,m)
+    implicit none
+    character(len=*), intent(in):: state
+    real*8, intent(in):: sigma
+    real*8, dimension(4), intent(out):: m
+
+
+    if ( state=='AF' ) then
+       m = [sigma, -sigma, -sigma, sigma]
+   else if (state=='SAF') then
+      m = [sigma, -sigma, sigma, -sigma]
+   else if (state=='PM') then
+      m = 0.d0
+   else if (state=='SD') then
+      m = [sigma, sigma, -sigma, -sigma]
+   end if
 
    end subroutine
 
    subroutine Ham_inter_state(state,J2,s1,s2,s3,s4,m_guess,Id_4,H_inter)
       implicit none
-            real*8, dimension(16,16), intent(in):: s1,s2,s3,s4, Id_4
-            integer :: i
-            real*8, intent(in):: J2
-            real*8, dimension(4), intent(in):: m_guess
-            character(len=3), intent(in):: state
-            real*8, dimension(16,16), intent(out):: H_inter
+      character(len=3), intent(in):: state
+      real*8, dimension(16,16), intent(in):: s1,s2,s3,s4, Id_4
+      real*8, intent(in):: J2, m_guess
+      real*8, dimension(16,16), intent(out):: H_inter
+
+      H_inter = 0.d0
 
       select case (state)
-      case ('AF')
-         do i = 1, 16
-            H_inter(i,i) = (-2*J1 + 3*J2)*(s1(i,i)-s2(i,i)-s3(i,i)+s4(i,i)-2*m_guess(1)*Id_4(i,i))*(m_guess(1)*Id_4(i,i))
-         enddo
-      case ('SAF')
-         do i = 1, 16
-            H_inter(i,i) = (-3*J2)*(s1(i,i)-s2(i,i)+s3(i,i)-s4(i,i)-2*m_guess(1)*Id_4(i,i))*(m_guess(1)*Id_4(i,i))
-         enddo
-      case ('SD')
-         do i = 1, 16
-            H_inter(i,i) = (-2*J1+J2)*(s1(i,i)+s2(i,i)-s3(i,i)-s4(i,i)-2*m_guess(1)*Id_4(i,i))*(m_guess(1)*Id_4(i,i))
-         enddo
-      case ('PM')
+       case ('AF')
+         H_inter = (-2*J1 + 3*J2)*(s1-s2-s3+s4-2*m_guess*Id_4)*(m_guess)
+
+       case ('SAF')
+         H_inter = (-3*J2)*(s1-s2+s3-s4-2*m_guess*Id_4)*(m_guess)
+
+       case ('SD')
+         H_inter = (-2*J1+J2)*(s1+s2-s3-s4-2*m_guess*Id_4)*(m_guess)
+
+       case ('PM')
          H_inter = 0
-      case default
+       case default
          print *, 'State inválido'
       end select
-      
-         end subroutine
+
+   end subroutine
 
 end module QUANTICO
