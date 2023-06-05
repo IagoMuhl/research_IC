6c73ddc92965e516ce77bce8d5644fd71ac0bd65
IagoMuhl <iagomuhl17@gmail.com>
2023-06-05 18:50:01 -0300

    J3=0.0|Gamma=0.1

--- Quantico/quant_J2-J3-gamma_fix.f90
+++ Quantico/quant_J2-J3-gamma_fix.f90
@@ -18,7 +18,7 @@ program quant_J2_J3_gamma
 
    H_1 = 0; H_2 = 0; W = 0; V = 0; dim = 2;
 
-   tol = 10.d0**(-8); Gamma = 0.d0
+   tol = 10.d0**(-8); Gamma = 1.d0
 !---------------------------------------------------------
 ! CALCULO DAS POSSIBILIDADES DE SIGMA-Z E IDENTIDADE
 
@@ -109,11 +109,11 @@ program quant_J2_J3_gamma
 !---------------------------------------------------------
 !DECLARAÇÃO DE VALORES INICIAIS
 
-         T = 0.9d0; 
+         T = 0.7d0; 
 
-         T_final = 2.d0;
+         T_final = 1.d0;
 
-         step = 10.d0**(-5); 
+         step = 10.d0**(-3); 
 
          m_guess = 1.d0;
       
