ab69fa052c5246233bda2987c20648338109fe59
IagoMuhl <iagomuhl17@gmail.com>
2023-06-12 15:01:40 -0300

    J3=0.1|Gamma=3

--- Quantico/quant_T-J3-gamma_fix.f90
+++ Quantico/quant_T-J3-gamma_fix.f90
@@ -2,7 +2,7 @@ program quant_T_J3_gamma
     use QUANTICO
     implicit none
  
-    real*8, parameter:: J3 = -0.1d0
+    real*8, parameter:: J3 = 0.1d0
     real*8, dimension(2,2):: sigma_z, Id, sigma_x
     real*8, dimension(2*2,2*2):: sig_zz, Id_2, Id_sig_z, sig_z_Id,Id_sigma_x, sigma_x_Id
     real*8, dimension(2**4,2**4):: F , H_1, H_2, H_intra, Id_4, H_inter, Ham, H_gama
@@ -18,7 +18,7 @@ program quant_T_J3_gamma
  
     H_1 = 0; H_2 = 0; W = 0; V = 0; dim = 2; 
  
-    tol = 10.d0**(-8); Gamma = 1.0d0
+    tol = 10.d0**(-8); Gamma = 3.0d0
  !---------------------------------------------------------
  ! CALCULO DAS POSSIBILIDADES DE SIGMA-Z E IDENTIDADE
  
@@ -110,11 +110,11 @@ program quant_T_J3_gamma
  !---------------------------------------------------------
  !DECLARAÇÃO DE VALORES INICIAIS
  
-          J2 = 0.45d0; 
+          J2 = 0.95d0; 
  
-          J2_final = 0.55d0;
+          J2_final = 0.90d0;
  
-          step = 10.d0**(-5); 
+          step = -10.d0**(-5); 
  
           m_guess = 1.d0;
        
@@ -131,7 +131,7 @@ program quant_T_J3_gamma
     !open(unit=20, file=trim(state) // "_T-F_J2(" // trim(adjustl(nameFileJ2)) // ")_J3(" // trim(adjustl(nameFileJ3)) // ").dat")
  !----------------------------------------------------
  
-    do while (J2 <= J2_final) !FUNÇÃO DE PARTIÇÃO/ LOOP TEMPERATURA
+    do while (J2 >= J2_final) !FUNÇÃO DE PARTIÇÃO/ LOOP TEMPERATURA
 
        ERRO = 1.d0
        
