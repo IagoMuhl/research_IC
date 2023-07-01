1ca0ec4f3fb7923ba143145b033cea79e9aed4a6
IagoMuhl <iagomuhl17@gmail.com>
2023-06-30 22:41:21 -0300

    J3=-0.2|Gamma=2

--- Quantico/quant_J2-J3-gamma_fix.f90
+++ Quantico/quant_J2-J3-gamma_fix.f90
@@ -2,7 +2,7 @@ program quant_J2_J3_gamma
    use QUANTICO
    implicit none
 
-   real*8, parameter:: J3 = 0.1d0
+   real*8, parameter:: J3 = -0.2d0
    real*8, dimension(2,2):: sigma_z, Id, sigma_x
    real*8, dimension(2*2,2*2):: sig_zz, Id_2, Id_sig_z, sig_z_Id,Id_sigma_x, sigma_x_Id
    real*8, dimension(2**4,2**4):: F , H_1, H_2, H_intra, Id_4, H_inter, Ham, H_gama
@@ -18,7 +18,7 @@ program quant_J2_J3_gamma
 
    H_1 = 0; H_2 = 0; W = 0; V = 0; dim = 2; 
 
-   tol = 10.d0**(-8); Gamma = 1.d0
+   tol = 10.d0**(-8); Gamma = 2.d0
 !---------------------------------------------------------
 ! CALCULO DAS POSSIBILIDADES DE SIGMA-Z E IDENTIDADE
 
@@ -110,11 +110,11 @@ program quant_J2_J3_gamma
 !---------------------------------------------------------
 !DECLARAÇÃO DE VALORES INICIAIS
 
-         T = 0.1d0; 
+         T = 0.3d0; 
 
-         T_final = 3.d0;
+         T_final = 4.5d0;
 
-         step = 10.d0**(-5); 
+         step = 10.d0**(-3); 
 
          m_guess = 1.d0;
       
